b31dc479c8b901e62b6f28f8fb8248e2d73cb5d3
akupferb <akupferb@umd.edu>
2019-11-18 12:15:12 -0500

    [src/TurtleBa.cpp] Added Class Source file with stubs

---
+++ src/TurtleBa.cpp
@@ -0,0 +1,23 @@
+
+#include "TurtleBa.hpp"
+
+TurtleBa::TurtleBa() {
+  
+}
+
+void TurtleBa::sensorCallback (const sensor_msgs::LaserScan::ConstPtr& data) {
+  
+}
+
+bool TurtleBa::isObstacle() {
+  
+}
+
+void TurtleBa::runAlgorithm() {
+  
+}
+
+TurtleBa::~TurtleBa() {
+  
+}
+
